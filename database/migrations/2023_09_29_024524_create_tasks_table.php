6da84bdc5c294a3721fbdabebc90edcd6cf0e272
asantusta <41saurab@gmail.com>
2023-10-06 20:00:22 +0545

    models

---
+++ database/migrations/2023_09_29_024524_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description')->nullable();
+            $table->string('file');
+            $table->date('dead_line');
+            $table->foreignId('faculty_id');
+            $table->foreignId('subject_id');
+            $table->foreignId('admin_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
